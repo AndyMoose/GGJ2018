de6a42036f7629195529615e90bb9cb357df8d48
ChooChuChu <33187653+ChooChuChu@users.noreply.github.com>
2018-01-28 03:21:20 -0500

    more assets

---
+++ Assets/Scripts/NPC_Sneezing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Sneezing : MonoBehaviour {
+
+    public int numberOfSneezes;
+    private int sneezesUsed;
+
+    private int timeBetweenSneezes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.Sneeze();
+    }
+
+    public void Sneeze()
+    {
+        timeBetweenSneezes = Random.Range(3, 10);
+        //if(NPCcontroller.)
+    }
+}
